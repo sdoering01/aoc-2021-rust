e675a6df314924ccc89cc8c6cfd811ac8d34b862
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2022-07-22 09:45:05 +0200

    add puzzle solution template

---
+++ template.rs
@@ -0,0 +1,29 @@
+use std::io::{Read, Result};
+
+type Input = ();
+
+fn read_input() -> Result<Input> {
+    let mut input_file = std::fs::File::open("input.txt")?;
+    let mut input = String::new();
+    input_file.read_to_string(&mut input)?;
+
+    // Parse input
+
+    Ok(())
+}
+
+fn task_1(input: Input) {
+    let solution = "todo";
+    println!("Task 1: {}", solution);
+}
+
+fn task_2(input: Input) {
+    let solution = "todo";
+    println!("Task 1: {}", solution);
+}
+
+fn main() {
+    let input = read_input().expect("error reading input");
+    task_1(input);
+    task_2(input);
+}
